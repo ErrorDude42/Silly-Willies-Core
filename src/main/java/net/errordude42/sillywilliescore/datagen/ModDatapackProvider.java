16b9573e5d55e50dc0f19dfad030a35a66d0723b
ErrorDude42 <whyqth0@gmail.com>
2025-07-17 19:49:04 -0500

    World gen setup

--- src/main/java/net/errordude42/sillywilliescore/datagen/ModDatapackProvider.java
+++ src/main/java/net/errordude42/sillywilliescore/datagen/ModDatapackProvider.java
@@ -2,6 +2,9 @@ package net.errordude42.sillywilliescore.datagen;
 
 import net.errordude42.sillywilliescore.SillyWilliesCore;
 import net.errordude42.sillywilliescore.trim.ModTrimMaterials;
+import net.errordude42.sillywilliescore.worldgen.ModBiomeModifiers;
+import net.errordude42.sillywilliescore.worldgen.ModConfiguredFeatures;
+import net.errordude42.sillywilliescore.worldgen.ModPlacedFeatures;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
@@ -9,6 +12,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceKey;
 import net.neoforged.neoforge.common.conditions.ICondition;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.List;
 import java.util.Map;
@@ -19,7 +23,11 @@ public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.TRIM_MATERIAL, ModTrimMaterials::bootstrap)
 
-            ;
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    ;
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(SillyWilliesCore.MOD_ID));
